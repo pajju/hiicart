52697510ad58e65292fddfcf05e03eb0abd04977
Hans Kuder <hans@Shosty.local>
2011-07-20 23:46:20 -0500

    Add initial express checkout test

---
+++ hiicart/tests/paypal_express_checkout.py
@@ -0,0 +1,12 @@
+import base
+
+class PaypalExpressCheckoutTestCase(base.HiiCartTestCase):
+    """Paypal Express Checkout tests"""
+    
+    def test_submit(self):
+        """Submit a cart to express checkout."""
+        self.assertEquals(self.cart.state, "OPEN")
+        result = self.cart.submit("paypal_express_checkout")
+        self.assertEqual(result.type, "url")
+        self.assertNotEquals(result.url, None)
+        self.assertEquals(self.cart.state, "SUBMITTED")
