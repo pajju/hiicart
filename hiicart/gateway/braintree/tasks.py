91571e9fc14b85c4d76a44a4b9b9929076ff8f14
Hans Kuder <hanskuder@gmail.com>
2011-09-22 16:00:57 -0500

    Add braintree celery update_payment_status task

---
+++ hiicart/gateway/braintree/tasks.py
@@ -0,0 +1,27 @@
+import logging
+
+from celery.decorators import task
+
+from hiicart.models import HiiCart
+from hiicart.gateway.braintree.ipn import BraintreeIPN
+
+log = logging.getLogger('hiicart.gateway.braintree.tasks')
+
+
+@task
+def update_payment_status(hiicart_id, transaction_id, tries=0):
+    """Check the payment status of a Braintree transaction."""
+    hiicart = HiiCart.objects.get(pk=hiicart_id)
+    handler = BraintreeIPN(hiicart)
+    done = handler.update_order_status(transaction_id)
+    # Reschedule the failed payment to run in 4 hours
+    if not done:
+        # After 18 tries (72 hours) we will void and fail the payment
+        if tries >= 18:
+            handler.void_order(transaction_id)
+        else:
+            tries = tries + 1
+            update_payment_status.apply_async(args=[hiicart_id,
+                                                    transaction_id,
+                                                    tries],
+                                              countdown=14400)
